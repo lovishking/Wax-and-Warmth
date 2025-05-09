9cd836df799a4c25070aa6ec61ff8b3bcee8a880
lovish <lovish@gmail.com>
2025-05-09 23:14:07 +0530

    okk

--- script.js
+++ script.js
@@ -204,3 +204,21 @@ function closeDialog() {
   document.getElementById("cartDialog").style.display = "none";
 }
 
+
+document.querySelectorAll('.p').forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    const rotateX = (y - centerY) / 15;
+    const rotateY = (x - centerX) / 15;
+    card.style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg) scale(1.03)`;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = `rotateX(0deg) rotateY(0deg) scale(1)`;
+  });
+});
+
